3d56a1e8ea3ef19a2190a6d7f70c5b7f76bb9151
raphaelrubia <raphaelrubiapereira@gmail.com>
2022-10-18 21:26:23 -0300

    Quarta alteração html, css e js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,66 @@
-alert('Seja bem vindo. Página em construção!');
+// Evento entrar cadastro
+const ImgLogin = document.querySelector('.button-login');
+const EntrarLogin = document.querySelector('.entrar-login');
+
+ImgLogin.addEventListener('click', function() {
+
+  if(EntrarLogin.style.display === 'flex') {
+    EntrarLogin.style.display = 'none';
+  } else {
+    EntrarLogin.style.display = 'flex'; 
+  }
+
+});
+
+const FecharLogin = document.querySelector('.fechar-login');
+const ConteinerEntrarLogin = document.querySelector('.entrar-login');
+
+FecharLogin.addEventListener('click', function() {
+
+  if(EntrarLogin.style.display === 'flex') {
+    EntrarLogin.style.display = 'none';
+  } else {
+    EntrarLogin.style.display = 'flex'; 
+  }
+
+});
+
+// Evento mostrar botão COMPRAR AGORA
+
+const GridVendas = document.querySelector('.grid-vendas');
+
+GridVendas.addEventListener('mouseover', function() {
+  
+ const BotaoComprar = document.querySelector('.botao-comprar');
+
+ BotaoComprar.style.display = 'flex';
+
+});
+
+GridVendas.addEventListener('mouseout', function() {
+  
+  const BotaoComprar = document.querySelector('.botao-comprar');
+ 
+  BotaoComprar.style.display = 'none';
+ 
+ });
+
+ const GridVendas2 = document.querySelector('.grid-vendas');
+
+GridVendas2.addEventListener('mouseover', function() {
+  
+ const BotaoComprar2 = document.querySelector('.botao-comprar');
+
+ BotaoComprar2.style.display = 'flex';
+
+});
+
+GridVendas2.addEventListener('mouseout', function() {
+  
+  const BotaoComprar2 = document.querySelector('.botao-comprar');
+ 
+  BotaoComprar2.style.display = 'none';
+ 
+ });
+
+ 
